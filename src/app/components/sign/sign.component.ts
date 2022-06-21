f8037e8edce1bb5ba0e0ec444245752e0ccd2f3d
nadiakramer <nadia.kramer@youengineering.ch>
2022-06-22 00:01:23 +0200

    button in sign optic and redesign overview screen

--- src/app/components/sign/sign.component.ts
+++ src/app/components/sign/sign.component.ts
@@ -10,11 +10,14 @@ import { fadeInOut } from 'src/app/util/fade-in-out.animation';
 })
 export class SignComponent implements OnInit {
   isDisplayed: boolean = false;
+  isTextDisplayed: boolean = false;
 
   constructor(private navigationService: NavigationService) {}
 
   ngOnInit(): void {
     setTimeout(() => (this.isDisplayed = true), 2000);
+    setTimeout(() => (this.isTextDisplayed = true), 2500);
+
   }
 
   onNavigateForward(): void {
